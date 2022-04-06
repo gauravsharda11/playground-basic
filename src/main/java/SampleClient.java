55852acc556e3c9407615f68c7c518344ab71620
Gaurav Sharda <61763472+gauravsharda11@users.noreply.github.com>
2022-04-06 16:06:19 -0400

    Initial Commit
    Tasks Done : 
    1. Modify SampleClient so that it prints the first and last name, and birth date of each Patient to the screen
    2. Sort the output so that the results are ordered by the patient's first name

---
+++ src/main/java/SampleClient.java
@@ -0,0 +1,58 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+import org.hl7.fhir.r4.model.Bundle;
+import org.hl7.fhir.r4.model.Patient;
+
+import ca.uhn.fhir.context.FhirContext;
+import ca.uhn.fhir.rest.client.api.IGenericClient;
+import ca.uhn.fhir.rest.client.interceptor.LoggingInterceptor;
+
+public class SampleClient {
+
+	public static void main(String[] theArgs) {
+
+		// Create a FHIR client
+		FhirContext fhirContext = FhirContext.forR4();
+		IGenericClient client = fhirContext.newRestfulGenericClient("http://hapi.fhir.org/baseR4");
+		client.registerInterceptor(new LoggingInterceptor(false));
+
+		List<Patient> patients = new ArrayList<Patient>();
+
+		// Search for Patient resources
+		Bundle response = client.search().forResource("Patient")
+				.where(Patient.FAMILY.matches().value("SMITH"))
+				.returnBundle(Bundle.class).execute();
+
+		response.getEntry().stream()
+		.forEach((e) -> {
+			patients.add((Patient) e.getResource());
+		});
+		
+		SampleClient.sortPatients(patients);
+		
+		for (Patient patient : patients) {
+			System.out.println(((Patient) patient).getNameFirstRep().getGivenAsSingleString() + " "
+					+ ((Patient) patient).getNameFirstRep().getFamily() + " ("
+					+ ((Patient) patient).getBirthDate() + ")");
+		}
+
+	}
+	
+	public static void sortPatients(List<Patient> patients) {
+		   Collections.sort(patients, new Comparator<Patient>() {
+		       public int compare(Patient p1, Patient p2) {
+		            int res =  p1.getNameFirstRep().getGivenAsSingleString()
+		            		.compareToIgnoreCase(p2.getNameFirstRep().getGivenAsSingleString());
+		            if (res != 0)
+		                return res;
+		            return p1.getNameFirstRep().getGivenAsSingleString()
+		            		.compareToIgnoreCase(p2.getNameFirstRep().getGivenAsSingleString());
+		       }
+		   });
+		}
+	
+	
+}
